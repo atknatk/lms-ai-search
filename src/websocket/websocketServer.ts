d5e875541f554eb9a9f7c6890190045bf3999738
Atakan Atik <atknatk@gmail.com>
2024-07-17 18:49:30 +0000

    refactor(websocketServer.ts): refactor initServer function to accept a server parameter for better modularity and readability

--- src/websocket/websocketServer.ts
+++ src/websocket/websocketServer.ts
@@ -3,27 +3,14 @@ import { handleConnection } from './connectionManager';
 import http from 'http';
 import { getPort } from '../config';
 import logger from '../utils/logger';
-import cors from 'cors'; // Import cors middleware
 
-const corsOptions = {
-  origin: ['https://dash.wodoxo.com', 'https://dash.abcenglishonline.com'],
-  methods: ['GET', 'POST'],
-};
-
-export const initServer = () => {
+export const initServer = (
+  server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>,
+) => {
   const port = getPort();
+  const wss = new WebSocketServer({ server });
 
-  // Create a separate HTTP server for CORS handling
-  const httpServer = http.createServer();
-  httpServer.listen(port, () => {
-    logger.info(`Server is running on port ${port}`);
-  });
-
-  // Apply CORS middleware to the HTTP server
-  httpServer.on('request', cors(corsOptions));
-
-  // Create WebSocket server attached to the HTTP server
-  const wss = new WebSocketServer({ server: httpServer });
   wss.on('connection', handleConnection);
+
   logger.info(`WebSocket server started on port ${port}`);
 };
